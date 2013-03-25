8a1d75c0d577f05d2040c715af17dc04d468b80c
John Garbutt <john@johngarbutt.com>
2013-03-25 17:54:15 +0000

    find the kernel version when loading ovs module

--- ovs.sh
+++ ovs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kernel_version=$(rpm -q kernel | grep 3.4)
+kernel_version=$( rpm -q kernel | grep 3.4 | sed "s/kernel-//" )
 insmod /lib/modules/${kernel_version}/kernel/net/openvswitch/openvswitch.ko
 
 ovsdb-server --remote=punix:/usr/var/run/openvswitch/db.sock \
