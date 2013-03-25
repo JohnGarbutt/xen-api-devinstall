f09766f67cdcabdc6a09257945564b80fc173ce6
John Garbutt <john@johngarbutt.com>
2013-03-25 17:50:09 +0000

    find the kernel version when loading ovs module

--- ovs.sh
+++ ovs.sh
@@ -1,4 +1,8 @@
-insmod /lib/modules/3.4.32-6.el6.centos.alt.x86_64/kernel/net/openvswitch/openvswitch.ko
+#!/usr/bin/env bash
+
+kernel_version=$(rpm -q kernel | grep 3.4)
+insmod /lib/modules/${kernel_version}/kernel/net/openvswitch/openvswitch.ko
+
 ovsdb-server --remote=punix:/usr/var/run/openvswitch/db.sock \
                      --remote=db:Open_vSwitch,manager_options \
                      --pidfile --detach
