ad5edaacfe8ebd9abce9910c279d55b7fe741b3a
John Garbutt <john@johngarbutt.com>
2013-03-25 16:09:28 +0000

    skip vncterm reinstall issues

--- build-utils.sh
+++ build-utils.sh
@@ -80,17 +80,17 @@ function xapi_deps_install {
     _install xen-devel libuuid-devel time pam-devel tk-devel libvirt-devel zlib-devel
 
     wget http://a94cd2de16980073c274-9e5915cce229bfd373f03bf01a9a7c85.r57.cf3.rackcdn.com/vncterm-1.6.10-251.x86_64.rpm
-    rpm -i vncterm-1.6.10-251.x86_64.rpm
+    rpm -i vncterm-1.6.10-251.x86_64.rpm || true
 
     wget http://a94cd2de16980073c274-9e5915cce229bfd373f03bf01a9a7c85.r57.cf3.rackcdn.com/eliloader -O /usr/bin/eliloader
     chmod +x /usr/bin/eliloader
 
     opam_config_env
 
-    opam install ocamlfind omake
+    opam install -y ocamlfind omake
 
-    opam remote add xen-dev git://github.com/xen-org/opam-repo-dev
-    opam install xen-api-libs-transitional stdext nbd tapctl libvhd oclock cdrom netdev xenopsd
+    opam remote -y add xen-dev git://github.com/xen-org/opam-repo-dev
+    opam install -y xen-api-libs-transitional stdext nbd tapctl libvhd oclock cdrom netdev xenopsd
 }
 
 function xapi_configure {
